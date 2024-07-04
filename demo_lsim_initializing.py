57d46c75ea8ba792912a7d7eb181e35c7179ac00
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2024-07-04 18:05:42 +0330

    plot Viterbi path was added plus some code commenting

--- demo_lsim_initializing.py
+++ demo_lsim_initializing.py
@@ -57,13 +57,13 @@ lsim_params.transition_matrices.loc[ch_names[1], ch_names[zee]] = np.array([[0.9
 # Init second LSIM channel with 2 states with 2-dim observations
 zee = 1
 
-# Init first state of first LSIM channel
+# Init first state of second LSIM channel
 i = 0; k = 0
 lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[-1],[-1]],dtype=np.float64)
 lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[0.1],[0.1]],dtype=np.float64)
 lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
 
-# Init second state of first LSIM channel
+# Init second state of second LSIM channel
 i = 1; k = 0
 lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[1],[1]],dtype=np.float64)
 lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[0.2],[0.2]],dtype=np.float64)
@@ -73,12 +73,9 @@ lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
 # Init init-state probabilities
 lsim_params.pi_0.loc[ch_names[zee]] = np.array([[0.25],[0.75]],dtype=np.float64)
 
-# Init C transition matrices for the first channel
+# Init C transition matrices for the second channel
 lsim_params.transition_matrices.loc[ch_names[0], ch_names[zee]] =  np.array([[0.97,0.03],[0.05,0.95],[0.01,0.99]],dtype=np.float64)
 lsim_params.transition_matrices.loc[ch_names[1], ch_names[zee]] = np.array([[0.98,0.02],[0.01,0.99]],dtype=np.float64)
-
-
-
 
 
 # set LSIM parameters with new values
@@ -87,8 +84,11 @@ lsim_init_determinstic.parameters = lsim_params
 # generate observation sequences from the model
 obs, latent_states = lsim_init_determinstic.generate_time_series(250) # generate 250 samples
 
+# performing Viterbi decoding
 P_star_model, X_star = lsim_init_determinstic.viterbi_lsim(obs)
 
 lsim_init_determinstic.plot_chmm_timeseries(obs, 1)
 lsim_init_determinstic.plot_chmm_timeseries(obs, 2)
-# obs1.columns = pd.MultiIndex.from_product([['obs:0001'],np.arange(obs1.columns.shape[0])])
+
+#plot Viterbi path for the first channel
+lsim_init_determinstic.plot_chmm_timeseries(X_star, 1)
