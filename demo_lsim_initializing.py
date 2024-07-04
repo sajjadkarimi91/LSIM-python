7dcfaaf95947029ab1a9301872c16693994bcf4f
Sajjad Karimi <50092872+sajjadkarimi91@users.noreply.github.com>
2024-07-04 11:31:52 +0330

    deterministic parameter initialization was finished

--- demo_lsim_initializing.py
+++ demo_lsim_initializing.py
@@ -16,41 +16,76 @@ lsim_params = lsim_init_determinstic.parameters
 
 C = lsim_params.C
 
-
-max_state_num = np.max(lsim_params.channel_state_num)
-max_gmm_num = np.max(lsim_params.num_gmm_component)
-dimension_numbers_reshape = np.concatenate([[0], np.cumsum(lsim_params.channel_obs_dim)])
-dimension_numbers_index = np.concatenate([[-1], np.cumsum(lsim_params.channel_obs_dim) - 1])
-state_numbers_index = np.concatenate([[-1], np.cumsum(lsim_params.channel_state_num) - 1])
-state_names = lsim_params.states_name_unique.copy()
-sum_states = state_numbers_index[-1] + 1
 ch_names = lsim_params.channels_name_unique
 state_names = lsim_params.states_name_unique
 gmm_names = lsim_params.gmm_names_unique
 
-
-lsim_params.coupling_theta_IM = np.array([[3,2],[1,8]]) # it is better coupling colomn be normalized to sum to one, but if not we normalize each colomn in next line
+# it is better coupling column be normalized to sum to one, but if not we normalize each column in next line
+lsim_params.coupling_theta_IM = np.array([[3,1],[3,2]])
 lsim_params.coupling_theta_IM = lsim_params.coupling_theta_IM / lsim_params.coupling_theta_IM.sum(axis=0, keepdims=1)
 
-# loop on all LSIM channels
-for zee in range(C):
-    for i in range(lsim_params.channel_state_num[zee]): #loop on each state per channel
+#######################################################################################
+# Init first LSIM channel with 3 states
+zee = 0
 
-        temp_sigma = 1
-        temp_mu = 1
-        P = 1
+# Init first state of first LSIM channel
+i = 0; k = 0
+lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([-1],dtype=np.float64)
+lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([0.1],dtype=np.float64)
+lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
 
-        for k in range(1, lsim_params.num_gmm_component[zee]):  #loop on each GMM per hidden state
-            lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = temp_mu[i, :]
-            lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = temp_sigma[i, :]
-            lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = P[i, :]
+# Init second state of first LSIM channel
+i = 1; k = 0
+lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([1],dtype=np.float64)
+lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([0.2],dtype=np.float64)
+lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
 
-        lsim_params.gmm_para_P.loc[ch_names[zee], state_names[i]] = 1
-        lsim_params.transition_matrices.loc[:, ch_names[zee]] = 1#  np.array([0,1;1,0])
+# Init third state of first LSIM channel
+i = 2; k = 0
+lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([3],dtype=np.float64)
+lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([0.15],dtype=np.float64)
+lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
+
+# Init init-state probabilities
+lsim_params.pi_0.loc[ch_names[zee]] = np.array([[0.25],[0.25],[0.5]],dtype=np.float64)
+
+# Init C transition matrices for the first channel
+lsim_params.transition_matrices.loc[ch_names[0], ch_names[zee]] = np.array([[0.98,0.01,0.01],[0.01,0.98,0.01],[0.1,0.1,0.8]],dtype=np.float64)
+lsim_params.transition_matrices.loc[ch_names[1], ch_names[zee]] = np.array([[0.98,0.01,0.01],[0.01,0.98,0.01]],dtype=np.float64)
+
+#######################################################################################
+# Init second LSIM channel with 2 states with 2-dim observations
+zee = 1
+
+# Init first state of first LSIM channel
+i = 0; k = 0
+lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[-1],[-1]],dtype=np.float64)
+lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[0.1],[0.1]],dtype=np.float64)
+lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
+
+# Init second state of first LSIM channel
+i = 1; k = 0
+lsim_params.gmm_para_mu.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[1],[1]],dtype=np.float64)
+lsim_params.gmm_para_sigma.loc[ch_names[zee], (state_names[i], gmm_names[k])] = np.array([[0.2],[0.2]],dtype=np.float64)
+lsim_params.gmm_para_P.loc[ch_names[zee], (state_names[i], gmm_names[k])] = 1
 
 
-# set LSIM parameters
+# Init init-state probabilities
+lsim_params.pi_0.loc[ch_names[zee]] = np.array([[0.25],[0.75]],dtype=np.float64)
+
+# Init C transition matrices for the first channel
+lsim_params.transition_matrices.loc[ch_names[0], ch_names[zee]] =  np.array([[0.97,0.03],[0.05,0.95],[0.01,0.99]],dtype=np.float64)
+lsim_params.transition_matrices.loc[ch_names[1], ch_names[zee]] = np.array([[0.98,0.02],[0.01,0.99]],dtype=np.float64)
+
+
+
+
+
+# set LSIM parameters with new values
 lsim_init_determinstic.parameters = lsim_params
 
 # generate observation sequences from the model
-lsim_init_determinstic.parameters = lsim_params
+obs, latent_states = lsim_init_determinstic.generate_time_series(250) # generate 250 samples
+lsim_init_determinstic.plot_chmm_timeseries(obs, 1)
+lsim_init_determinstic.plot_chmm_timeseries(obs, 2)
+# obs1.columns = pd.MultiIndex.from_product([['obs:0001'],np.arange(obs1.columns.shape[0])])
